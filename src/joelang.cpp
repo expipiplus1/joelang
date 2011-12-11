95b05844d28e60572ead0817c3fec6d65ddb23a5
Joe Hermaszewski <expipiplus1@gmail.com>
2011-12-11 23:29:38 +0000

    [-] All Gone

--- src/joelang.cpp
+++ src/joelang.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
-#include <string>
-
-#include "declarations.hpp"
-#include "driver.hpp"
-#include "parsingcontext.hpp"
 
 int main( int argc, char** argv )
 {
-    JoeLang::ParsingContext parsing_context;
-    JoeLang::Driver driver( parsing_context );
-
-    if( !driver.parse_string( std::string( "technique{pass{a=b;b=c;}pass{{}}}\n" ) ) )
-        std::cout << "Couldn't parse\n";
-    else
-        std::cout << parsing_context.GetDeclarationSeq()->GetDeclarations().size() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 }
