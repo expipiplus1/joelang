d27382dffde74d573ccb6f2198f005c2fe7e7115
Joe Hermaszewski <expipiplus1@gmail.com>
2011-12-23 21:13:54 +0000

    [+] Created StateAssignment class

---
+++ src/state_assignment.hpp
@@ -0,0 +1,60 @@
+/*
+    Copyright 2011 Joe Hermaszewski. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are
+    permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of
+    conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list
+    of conditions and the following disclaimer in the documentation and/or other materials
+    provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY JOE HERMASZEWSKI "AS IS" AND ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JOE HERMASZEWSKI OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those of the
+    authors and should not be interpreted as representing official policies, either expressed
+    or implied, of Joe Hermaszewski.
+*/
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include "expression.hpp"
+#include "token.hpp"
+
+namespace JoeLang
+{
+
+namespace Parser
+{
+
+class Parser;
+
+class StateAssignment : public JoeLang::Parser::Token
+{
+public:
+    virtual ~StateAssignment();
+
+    static bool Parse( Parser& parser, std::unique_ptr<StateAssignment>& token );
+
+protected:
+    StateAssignment( std::string state_name, std::unique_ptr<Expression> expression );
+
+private:
+    std::string m_stateName;
+    std::unique_ptr<Expression> m_expression;
+};
+
+} // namespace Parser
+} // namespace JoeLang
