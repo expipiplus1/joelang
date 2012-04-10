202e1d1f7d57e7f4160ac7fc5d846d4719379336
Joe Hermaszewski <expipiplus1@gmail.com>
2012-04-10 16:31:25 +0100

    [~] Cleaned up lexer

--- src/compiler/lexer.hpp
+++ src/compiler/lexer.hpp
@@ -37,42 +37,102 @@ namespace Compiler
 
 enum class TerminalType;
 
-//------------------------------------------------------------------------------
-// Lexer
-//------------------------------------------------------------------------------
-
+/**
+  * \class Lexer
+  * \brief A class to hold onto a source string and produce tokens of it
+  */
 class Lexer
 {
 public:
-                 Lexer           () = delete;
+    /**
+      * \param string
+      *   The string for the whole proprocessed soruce file
+      */
                  Lexer           ( std::string string );
                  ~Lexer          () = default;
 
-    //
-    // Expect return true and moves the position forward if the next terminal
-    // matches terminal_type. It optionally returns the matched string
-    //
+    /**
+      * This function advances the stream by one token if terminal_type is the
+      * next token in the stream, otherwise the stream doesn't move
+      * \param terminal_type
+      *   The TerminalType to look for
+      * \returns whether terminal_type was the next terminal in the stream
+      */
     bool         Expect          ( TerminalType terminal_type );
+    /**
+      * This function advances the stream by one token if terminal_type is the
+      * next token in the stream, otherwise the stream doesn't move
+      * \param terminal_type
+      *   The TerminalType to look for
+      * \param string
+      *   If terminal_type was found, fill string with the parsed characters
+      * \returns whether terminal_type was the next terminal in the stream
+      */
     bool         Expect          ( TerminalType terminal_type,
                                    std::string& string );
 
-    TerminalType GetNextTerminal ( std::string& string );
+    /**
+      * \param string
+      *   The characters belonging to the next terminal
+      * \returns
+      *   The TerminalType of the next token in the stream
+      */
+    TerminalType PeekNextTerminal ( std::string& string ) const;
 
+    /** \returns The position in characters from the start of the string **/
     std::size_t  GetPosition     () const;
+    /** \returns The line numer in the currenst string **/
     std::size_t  GetLineNumber   () const;
+    /** \returns The number of characters since the last newline + 1 **/
     std::size_t  GetColumnNumber () const;
 
 private:
+    /** Consumes terminals until a non-whitespace and non-comment one **/
     void         ConsumeIgnoredTerminals ();
+
+    /**
+      * Tries to read a punctuation terminal
+      * \param terminal
+      *   The TerminalType to look for
+      * \param string
+      *   The string to fill with the parsed characters on a sucessful parse
+      * \returns UNKNOWN_CHARACTER if it didn't parse a token
+      *   Otherwise the TerminalType parsed
+      */
     TerminalType ReadPunctuationTerminal ( TerminalType terminal,
-                                           std::string& string );
+                                           std::string& string ) const;
+    /**
+      * Tries to read a literal terminal
+      * \param terminal
+      *   The TerminalType to look for
+      * \param string
+      *   The string to fill with the parsed characters on a sucessful parse
+      * \returns UNKNOWN_CHARACTER if it didn't parse a token
+      *   Otherwise the TerminalType parsed
+      */
     TerminalType ReadLiteralTerminal     ( TerminalType terminal,
-                                           std::string& string );
+                                           std::string& string ) const;
+    /**
+      * Tries to read a keyword terminal
+      * \param terminal
+      *   The TerminalType to look for
+      * \param string
+      *   The string to fill with the parsed characters on a sucessful parse
+      * \returns UNKNOWN_CHARACTER if it didn't parse a token
+      *   Otherwise the TerminalType parsed
+      */
     TerminalType ReadKeywordTerminal     ( TerminalType terminal,
-                                           std::string& string );
+                                           std::string& string ) const;
+    /** Advances the position in the stream by num_chars.
+      * It also keeps track of the line and column numbers
+      * \param num_chars
+      *   The number of characters by which to advance the position
+      */
     void         ReadChars               ( std::size_t num_chars );
 
+
     const std::string           m_string;
+
     std::string::const_iterator m_position;
     std::size_t                 m_lineNumber = 1;
     std::size_t                 m_columnNumber = 1;
